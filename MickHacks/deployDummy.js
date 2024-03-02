687078f1e9c84e8a5e9062c6705e0947f24d2580
Mickeena <51884041+Mickeena@users.noreply.github.com>
2024-03-02 03:35:52 +0000

    Converted [deployDummy] console log
    Converted to printed console log and suppressed default logs.

--- MickHacks/deployDummy.js
+++ MickHacks/deployDummy.js
@@ -7,10 +7,10 @@
 /*  private servers.						 */
 /* ----------------------------------------- */
 
-import { scanAll, nukeAll, mapAll } from "utility/eye.js";
+import { scanAll, nukeAll } from "utility/eye.js";
 
 export async function main(ns) {
-
+	ns.disableLog("All");
 	// Initialise
 	const deployFile = "MickHacks/mhd.js";
 	if (!ns.fileExists(deployFile)) {
@@ -22,20 +22,23 @@ export async function main(ns) {
 	let countThreads = 0
 	let countWeak = 0
 	// Scan and nuke all targets
+	ns.print("Calling server scan")
 	let serverList = scanAll(ns)
+	ns.print("Calling server nuke")
 	await nukeAll(ns)
-
-
+	ns.print("Starting server list sort")
 	for (const server of serverList) {
 		// Skip if the server is "home" or starts with "MickServ"
 		if (server === "home" || server.startsWith("MickServ")) {
+			ns.print(`Skipped: ${server}, owned server.`)
 			continue;
 		}
 		// Skip if server has no root access
 		if (!ns.hasRootAccess(server)) {
+			ns.print(`Skipped: ${server}, no root access.`)
 			continue;
 		}
-
+		ns.print(`Initiating deployment on: ${server}`)
 		// Kill any already running scripts on the server
 		ns.killall(server);
 		// Get RAM of the rooted server
@@ -47,14 +50,17 @@ export async function main(ns) {
 			// Copy and run a script on the rooted server
 			ns.scp(deployFile, server);
 			ns.exec(deployFile, server, serverThreads.toString());
+			ns.print(`Deployed to: ${server}, with ${serverThreads} threads.`)
 			countServs++
 			countThreads = countThreads + serverThreads
 		} else {
 			countWeak = countWeak++
+			ns.print(`Server ${server} too weak to deploy.`)
 		}
 	}
-		ns.tprintf("")
-		ns.tprintf("\x1b[38;5;166mTotal servers too weak: " + countWeak + "\x1b[0m"); // Whiteish
-		ns.tprintf("\x1b[38;5;250mTotal servers deployed: " + countServs + "\x1b[0m"); // Whiteish
-		ns.tprintf("\x1b[38;5;250mTotal threads used: " + countThreads + "\x1b[0m"); // Whiteish
+	ns.print(`All servers deployed, exiting script.`)
+	ns.tprintf("")
+	ns.tprintf("\x1b[38;5;166mTotal servers too weak: " + countWeak + "\x1b[0m"); // Whiteish
+	ns.tprintf("\x1b[38;5;250mTotal servers deployed: " + countServs + "\x1b[0m"); // Whiteish
+	ns.tprintf("\x1b[38;5;250mTotal threads used: " + countThreads + "\x1b[0m"); // Whiteish
 }
