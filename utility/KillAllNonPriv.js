e2000345c6e0f6f775860ee49500b98e520620c1
Mickeena <51884041+Mickeena@users.noreply.github.com>
2024-03-02 20:19:42 +0000

    Converted [KillAllNonPriv] console log
    Converted to printed console log and suppressed default logs.

--- utility/KillAllNonPriv.js
+++ utility/KillAllNonPriv.js
@@ -9,21 +9,25 @@ import { scanAll } from "utility/eye.js";
 
 export async function main(ns) {
 	// Get the list of all servers
+	ns.disableLog("ALL");
+	ns.print(`Listing all servers`)
     const serverList = scanAll(ns); // Get all servers
 
     // Get the list of purchased servers
+	ns.print(`Listing all purchased servers`)
     const purchasedServers = await ns.getPurchasedServers();
 
     // Filter servers based on the criteria
+	ns.print(`Filtering list`)
     const serversToKill = serverList.filter(server => {
         // Exclude servers that are in the list of purchased servers
         // Also exclude the "home" server
         return server !== "home" && !purchasedServers.includes(server);
     });
-
+	ns.print(`List filtered. Killing scripts.`)
     // Iterate through the filtered servers and kill all scripts
     for (const server of serversToKill) {
         ns.killall(server);
-        ns.tprint(`Killed all scripts on server: ${server}`);
+        ns.print(`Killed all scripts on server: ${server}`);
     }
 }
