d9c4834f8925ce697aa701c2cb94c031750d8033
Mickeena <51884041+Mickeena@users.noreply.github.com>
2024-03-03 01:28:55 +0000

    Update startup.js
    Rejiggered positioning of tails
    Removed unnecessary tail for deploying dummy scripts
    Upped default hacknet to 30 nodes

--- startup.js
+++ startup.js
@@ -8,23 +8,17 @@
 export async function main(ns) {
 	// Execute the deployDummy.js script and tail its output
 	const deployDummyPid = ns.exec('MickHacks/deployDummy.js', 'home', 1);
-	//await ns.sleep(0);
-	ns.tail(deployDummyPid);
-	ns.moveTail(950,76, deployDummyPid)
-	ns.resizeTail(300,500, deployDummyPid)
 
 	// Execute the hacknet.js script with 24 as an argument and tail its output
-	const hacknetPid = ns.exec('utility/hacknet.js', 'home', 1, 24);
-	//await ns.sleep(0);
+	const hacknetPid = ns.exec('utility/hacknet.js', 'home', 1, 30);
 	ns.tail(hacknetPid);
-	ns.moveTail(950,39, hacknetPid)
+	ns.moveTail(950,37, hacknetPid)
 	ns.resizeTail(300,500, hacknetPid)
 
 	// Execute the purchasePrivate.js script with different arguments and tail its output
 	let privatePid = ns.exec('utility/purchasePrivate.js', 'home', 1, 1024);
-	//await ns.sleep(0);
 	ns.tail(privatePid);
-	ns.moveTail(950,2, privatePid)
+	ns.moveTail(950,1, privatePid)
 	ns.resizeTail(300,500, privatePid)
 
 	while (true) {
@@ -38,7 +32,7 @@ export async function main(ns) {
 	privatePid = ns.exec('utility/purchasePrivate.js', 'home', 1, 16384);
 	//await ns.sleep(0);
 	ns.tail(privatePid);
-	ns.moveTail(950,2, privatePid)
+	ns.moveTail(950,1, privatePid)
 	ns.resizeTail(300,500, privatePid)
 	while (true) {
 		if (!ns.isRunning(privatePid)) {
@@ -51,7 +45,7 @@ export async function main(ns) {
 	privatePid = ns.exec('utility/purchasePrivate.js', 'home', 1, 1048576);
 	//await ns.sleep(0);
 	ns.tail(privatePid);
-	ns.moveTail(950,2, privatePid)
+	ns.moveTail(950,1, privatePid)
 	ns.resizeTail(300,500, privatePid)
 	while (true) {
 		if (!ns.isRunning(privatePid)) {
