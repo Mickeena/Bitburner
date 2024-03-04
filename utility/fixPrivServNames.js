64a71038b3a2df3d98705d9f64a32d2f165c0da3
Mickeena <51884041+Mickeena@users.noreply.github.com>
2024-03-04 01:53:13 +0000

    Update fixPrivServNames.js
    Updated to work as an exported function without breaking

--- utility/fixPrivServNames.js
+++ utility/fixPrivServNames.js
@@ -12,21 +12,13 @@ export async function main(ns) {
     await fixPrivServNames(ns);
 }
 
-export async function fixPrivServNames(ns) {
-    // Check for excess args
-    if (ns.args.length !== 0 && ns.args.length !== 1) {
-        ns.tprintf("\x1b[31mUsage: run utility/fixPrivServNames.js <serverName>");
-        return;
-    }
-
+export async function fixPrivServNames(ns, targetServer = null) {
     // Define the naming pattern
     const namingPattern = /^MickServ-(?:[1-9]|1\d|2[0-5])$/;
 
-    // Check if an argument is provided
-    const args = ns.args;
-    if (args.length > 0) {
-        // Argument provided, remove the specified server
-        const targetServer = args[0];
+    // Check if a targetServer is provided
+    if (targetServer !== null) {
+        // Target server provided, remove the specified server
         if (ns.getPurchasedServers(true).includes(targetServer)) {
             ns.tprintf(`\x1b[38;5;155mRemoving server "${targetServer}" as requested.`);
             await ns.deleteServer(targetServer);
@@ -34,7 +26,7 @@ export async function fixPrivServNames(ns) {
             ns.tprintf(`\x1b[31mServer "${targetServer}" either does not exist or is not a private server.`);
         }
     } else {
-        // No argument provided, check all purchased servers
+        // No targetServer provided, check all purchased servers
         const purchasedServers = ns.getPurchasedServers();
         for (const serverName of purchasedServers) {
             // Check if the server name matches the naming pattern
