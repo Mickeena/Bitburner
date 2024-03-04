3c45153b36894eb55786abcbf3765c770441059a
Mickeena <51884041+Mickeena@users.noreply.github.com>
2024-03-04 06:47:51 +0000

    Update startup.js
    Added singularity stuff

--- startup.js
+++ startup.js
@@ -5,9 +5,50 @@
 /*  augment installation					 */
 /* ----------------------------------------- */
 
+import { formatValue, formatValueTime, curTime, formatValueSec } from 'utility/formatValues.js';
+
 export async function main(ns) {
 
+	const programs = ns.singularity.getDarkwebPrograms();
+	let funds = ns.getServerMoneyAvailable("home"); // Initialize funds
+
+	if (!ns.hasTorRouter()) {
+		while (true) {
+			const torCost = 200000;
+			ns.print(`Awaiting funds to purchase Tor Router for $200,000`)
+			if (funds > torCost) {
+				ns.singularity.purchaseTor();
+				ns.print(`Purchased tor router.`)
+				ns.tprintf(`Purchased tor router.`)
+				break; // Exit the loop once Tor Router is purchased
+			} else {
+				await ns.sleep(10000); // Wait for 10 seconds before checking funds again
+				funds = ns.getServerMoneyAvailable("home");
+			}
+		}
+	}
+
+	for (const program of programs) {
+		const programCost = ns.singularity.getDarkwebProgramCost(program);
+		const programCostF = formatValue(programCost)
+		if (!ns.fileExists(program)) {
+			ns.print(`Awaiting funds to purchase: ${program} for $${programCostF}`)
+			while (true) {
+				if (funds > programCost) {
+					ns.singularity.purchaseProgram(program);
+					ns.tprintf(`Purchased program ${program}`);
+					ns.print(`Purchased program ${program}`);
+					break; // Exit the while loop once the program is purchased
+				} else {
+					await ns.sleep(10000);
+					funds = ns.getServerMoneyAvailable("home");
+				}
+			}
+		}
+	}
+
 	ns.killall()
+	
 
 	// Deploy dummy hacking script to a;ll public servers
 	const deployDummyPid = ns.exec('MickHacks/deployDummy.js', 'home', 1);
